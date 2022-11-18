d66829b1801ba41760628e9840f0175ee3b530eb
Jiteshkumar yadav <9766jitesh@gmail.com>
2022-11-18 15:27:53 +0530

    solution

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,11 @@
 import React, { Children } from 'react'
 
 export default function Welcome(props) {
+  let greetMsg='Welcome to Newton School.';
   return (
     <div>
-      <h1>Hey !{props.name}.</h1>
-      <h2>Welcome to Newton School.</h2>
+      <h1>Hey {props.name}!.</h1>
+      <h2>{greetMsg}</h2>
     </div>
   )
 }
